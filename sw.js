d8710f5ba6650d4fe1d25f67ba3a042b154d2122
Barla Fam <nd4zmoe@gmail.com>
2026-04-23 12:18:06 +0700

    Update service worker version to 1.5

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /**
  * SERVICE WORKER keuanganNILA (ENTERPRISE SECURITY & CACHE LIMIT)
- * Versi 1.2
+ * Versi 1.5
  */
 
-const APP_VERSION = '1.2'; 
+const APP_VERSION = '1.5'; 
 const CACHE_PREFIX = 'keuangan-nila-';
 const CACHE_STATIC = CACHE_PREFIX + 'static-v' + APP_VERSION;
 const CACHE_DYNAMIC = CACHE_PREFIX + 'dynamic-v' + APP_VERSION;
